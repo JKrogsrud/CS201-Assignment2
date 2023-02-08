dfd9d9d4036b2bbf1e4a8c77af82153c0e784e16
Jared Krogsrud <JKrogsrud@gmail.com>
2023-02-08 13:35:50 -0500

    files setup for work

---
+++ linkedlist-main.jkrogsru.c
@@ -0,0 +1,20 @@
+//
+// Created by Jared Krogsrud on 2/8/2023.
+//
+
+/*
+ * main will open a file for which the name is specified on
+ * command line; read a line from the file, check that the
+ * line is valid, perform the action specified by the line,
+ * and then repeat; until all lines have been read.
+ *
+ * Every time a file is successfully read, print a message
+ * describing the action taken or error message if not successful.
+ * Print an error message if an input line does not have correct format.
+ *
+ */
+int main()
+{
+
+}
+
